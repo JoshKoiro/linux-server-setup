9207d4dfdedd9bf91f0c7d7edf73e59c474c0dca
Josh <josh@localhost.localdomain>
2024-09-27 22:20:10 -0400

    added -y tab to install commands

--- installScripts/vivaldi.sh
+++ installScripts/vivaldi.sh
@@ -2,7 +2,7 @@
 # This script comes from the following website: https://itsfoss.com/install-vivaldi-ubuntu-linux/
 
 # Install wget
-sudo apt install wget
+sudo apt install wget -y
 
 # Get public key of the Vivaldi repository
 wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor | sudo dd of=/usr/share/keyrings/vivaldi-browser.gpg
@@ -11,4 +11,4 @@ wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor
 echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=$(dpkg --print-architecture)] https://repo.vivaldi.com/archive/deb/ stable main" | sudo dd of=/etc/apt/sources.list.d/vivaldi-archive.list
 
 # Update the package and install
-sudo apt update && sudo nala install vivaldi-stable
+sudo apt update && sudo nala install vivaldi-stable -y
